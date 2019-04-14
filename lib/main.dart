3e833b8a9e29ac9609d1f23376ed44fb314be411
nishang.jx <nishang.jx@autonavi.com>
2019-04-14 23:54:35 +0800

    init
    初始化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+import 'package:flutter/rendering.dart';
+void main() {
+debugPaintSizeEnabled=true;
+// debugPaintBaselinesEnabled = true;
+//debugPaintLayerBordersEnabled = true;
+runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
